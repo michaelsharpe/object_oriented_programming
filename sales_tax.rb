85b4f7a5aa08d730f4646c10a2bdb073eb4ae179
michaelsharpe <m.sharpe@project89.org>
2013-10-22 17:59:57 -0400

    update item class

--- sales_tax.rb
+++ sales_tax.rb
@@ -3,24 +3,53 @@ class Item
 
   def initialize(type, price, imported)
     @type = type
-    @price = price.to_i
-    @imported = imported ||= false
+    @price = price
+    @imported = imported
+    @tax = 0.1 
   end
 
-  def tax
+  def tax_percent
     case @type
     when :book, :food, :medical 
       if @imported == true 
-        @price * 0.15
+        @tax = 0.5
       else
-        @price * 0.1
+        @tax = 0
       end
     else
       if @imported == true
-        @price * 0.5
+        @tax = 0.15
       end
     end
+    @tax
+  end
+
+  def tax_total
+    self.tax_percent
+    tax = @tax * @price / 100
+
+    #round tax to nearest 0.05
+    if tax % 0.05 != 0
+      tax +=  0.05 - (tax % 0.05)
+    end
+
+    return tax
+  end
+
+  def total_price
+    self.tax_total + @price
   end
 
 end
 
+book = Item.new(:book, 12.49, false)
+puts book.total_price
+puts book.tax_percent
+puts book.tax_total
+chocolates = Item.new(:food, 10.00, true)
+puts chocolates.price
+puts chocolates.type
+puts chocolates.imported
+puts chocolates.total_price
+puts chocolates.tax_percent
+puts chocolates.tax_total
