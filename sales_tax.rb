0818b029a0d7cdb14692ed33e4fd44f478ffe3c0
michaelsharpe <m.sharpe@project89.org>
2013-10-23 14:30:04 -0400

    add receipt

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,8 +1,9 @@
 class Product
-  attr_accessor :name, :type, :price, :imported
+  attr_accessor :name, :quantity, :type, :price, :imported
 
-  def initialize(name, type, price, imported)
+  def initialize(name, quantity, type, price, imported=false)
     @name = name
+    @quantity = quantity
     @type = type
     @price = price
     @imported = imported
@@ -16,14 +17,16 @@ class CashRegister
   @@import = 0.05
 
   def initialize
-    @item_list=[]
-    @tax_list = []
+    @item_list = []
+    @tax_total = 0
+    @total = 0
   end
 
   def scan(*items)
-    items.each do |x| 
-      @item_list << x
-      @tax_list << calc_tax(x)
+    items.each do |item| 
+      @item_list << item
+      @tax_total += calc_tax(item)
+      @total += calc_total(item)
     end
   end
 
@@ -34,30 +37,36 @@ class CashRegister
     @tax_rate
   end
 
-
   def calc_tax(item)
     tax_total = item.price * tax_rate(item)
     tax_total +=  0.05 - (tax_total % 0.05) if  tax_total % 0.05 != 0
-    tax_total
+    tax_total.round(2)
+  end
+
+  def calc_total(item)
+    item.price + calc_tax(item)
   end
 
   def receipt
+    printout = String.new
+    @item_list.each do |item|
+      printout << "#{item.quantity} #{item.name}: #{calc_total(item)} \n"
+    end
+    printout << "Sales Taxes: #{@tax_total} Total: #{@total}"
+    #reset instance variables for next scan
+    @item_list = []
+    @tax_total = 0
+    @total = 0
 
-
+    p printout
   end
 
 end
 
-book=Product.new("book", "book", 12.49, false)
-wine=Product.new("wine", "alchohol", 15.00, true)
+book = Product.new("book", 1, "book", 12.49)
+cd = Product.new("music CD", 1, "cd", 14.99)
+bar = Product.new("chocolate bar", 1, "food", 0.85)
+
+
 
 register= CashRegister.new
-
-puts book.type
-puts book.price
-puts register.tax_rate(book)
-puts register.tax_rate(wine)
-
-register.scan(book, wine)
-puts register.item_list
-puts register.tax_list
